81b201f7958d9045e49de327899f1b55f791fe78
Jared Beard <jbeard6@mix.wvu.edu>
2020-07-06 10:57:51 -0400

    Initial Commit

---
+++ state_machine/src/sm_rd1_node.cpp
@@ -0,0 +1,23 @@
+/**
+@file example_sm_node.cpp
+Runs the round 1 state machine for the SRC2 competition
+@author Jared Beard, Nick Ohi
+@version 1.0 7/5/2020
+*/
+
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+
+  // Initialize ROS, Subs, and Pubs *******************************************
+ros::init(argc, argv, "state_machine_rd1_node");
+
+ros::NodeHandle nh;
+
+
+
+
+
+  return 0;
+}
