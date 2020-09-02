7919233a6d0f8dcafbd24c8073ee1f4d1ceca8f1
jsngross <jason.gross@mail.wvu.edu>
2020-09-02 12:19:23 -0400

    brake in wrong place while homing

--- state_machine/src/sm_rd1.cpp
+++ state_machine/src/sm_rd1.cpp
@@ -1136,17 +1136,7 @@ void SmRd1::stateLost()
   {
     ROS_ERROR("Failed to call service Stop");
   }
-  srcp2_msgs::BrakeRoverSrv srv_brake;
-  srv_brake.request.brake_force  = 500.0;
-  if (clt_srcp2_brake_rover_.call(srv_brake))
-  {
-     ROS_INFO_STREAM("SM: Brake Enabled? "<< srv_brake.response.finished);
 
-  }
-  else
-  {
-      ROS_ERROR("Failed to call service Brake");
-  }
 
   // Turn on the Lights
   srcp2_msgs::ToggleLightSrv srv_lights;
@@ -1187,6 +1177,17 @@ void SmRd1::stateLost()
   else
   {
     ROS_ERROR("Failed to call service ApproachBaseStation");
+  }
+  srcp2_msgs::BrakeRoverSrv srv_brake;
+  srv_brake.request.brake_force  = 500.0;
+  if (clt_srcp2_brake_rover_.call(srv_brake))
+  {
+     ROS_INFO_STREAM("SM: Brake Enabled? "<< srv_brake.response.finished);
+
+  }
+  else
+  {
+      ROS_ERROR("Failed to call service Brake");
   }
 
   // Homing - Measurement Update
