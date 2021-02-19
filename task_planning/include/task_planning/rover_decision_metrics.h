fd5adef84f12de8351246be28c0b2e0a053fa94f
Jared Beard <jbeard6@mix.wvu.edu>
2021-02-19 14:56:35 -0500

    Added some details to framework

---
+++ task_planning/include/task_planning/rover_decision_metrics.h
@@ -0,0 +1,7 @@
+/*
+   Rover Decision Metrics
+   rover_decision_metrics.h
+   Purpose: Presents a class of decision metrics for allocating rovers
+   @author Jared Beard
+   @version 1.0 2/19/21
+ */
--- task_planning/src/scouting_task_node.cpp
+++ task_planning/src/scouting_task_node.cpp
@@ -1,7 +1,7 @@
 /*
-   Excavation Task Node
-   excavation_task_node.cpp
-   Purpose: Allocates excavators and haulers to perform excavations related tasks
+   Scouting Task Node
+   scouting_task_node.cpp
+   Purpose: Allocates scouts to perform search related tasks
    @author Jared Beard
    @version 1.0 2/19/21
  */
@@ -13,6 +13,8 @@ int main(int argc, char** argv)
 		ros::init(argc,argv,"scouting_task_node");
 		ros::NodeHandle nh;
 
+    /**----------------- Import Offline Plan -----------------------------*/
+
     /**----------------- Import Parameters -----------------------------*/
 
     /**----------------- Initialize Pubs/Subs -----------------------------*/
@@ -20,17 +22,13 @@ int main(int argc, char** argv)
 		//ros::Publisher pub = n.advertise<std_msgs::Int64MultiArray>("/motor_pos", 1000);
     //ros::Publisher pubCurrent = n.advertise<std_msgs::Int64MultiArray>("/motor_currents", 1000);
 
-    /**----------------- Initialize Volatile Metric Class------------------------------*/
 
     /**----------------- Initialize Rover Metric Class------------------------------*/
 
     while(ros::ok())
     {
-      /**----------------- Update Volatile Cost ------------------------------*/
 
-      /**----------------- Update Excavator Cost ------------------------------*/
-
-      /**----------------- Update Hauler Cost ------------------------------*/
+      /**----------------- Update Rover Cost ------------------------------*/
 
       /**----------------- Perform Online Planning ------------------------------*/
       // This could be a part of the cost steps.
